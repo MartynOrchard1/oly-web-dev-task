7e4a6bd0c52a27cef8aca848754c0a6ac3f95b7d
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-07-19 11:41:02 +1200

    #3 Fetch the dog image from provided api mentioned in the git issue

--- oly/src/App.vue
+++ oly/src/App.vue
@@ -9,3 +9,23 @@
   </div>
 </template>
 
+<script setup>
+import { ref, onMounted } from 'vue'
+
+const dogImage = ref('')
+
+const fetchDogImage = async () => {
+  try {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await response.json()
+    dogImage.value = data.message
+  } catch (error) {
+    console.error('Error fetching dog image:', error)
+  }
+}
+
+onMounted(() => {
+  fetchDogImage()
+})
+</script>
+
