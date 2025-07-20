71ed44e49a26fdfe3b337825aea6908373d7731b
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-07-20 22:25:11 +1200

    Create CreateDog.vue

---
+++ oly/src/components/CreateDog.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="form-container">
+        <h2>Create a Dog</h2>
+        <form @submit.prevent="handleSubmit">
+            <label for="name">Dog Name</label>
+            <input type="text" id="name" v-model="dogName" required />
+        </form>
+    </div>
+</template>
