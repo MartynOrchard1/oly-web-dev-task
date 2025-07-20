6b6e55ff71c383854d8ce113c76723c7dd5dd646
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-07-20 21:30:37 +1200

    #8 Quick error check to make sure there's dogs in the list

--- oly/src/components/SavedDogs.vue
+++ oly/src/components/SavedDogs.vue
@@ -2,6 +2,10 @@
     <div class="saved-seciton">
         <h2>Saved Dogs</h2>
 
+        <div v-if="dogs.length === 0">
+            <p>No Saved dogs</p>
+        </div>
+
         
     </div>
 </template>
