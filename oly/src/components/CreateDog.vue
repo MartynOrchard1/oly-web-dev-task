43e61f07bc41c54467f937a43d9916afbffe4c18
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-07-20 22:25:36 +1200

    Update CreateDog.vue

--- oly/src/components/CreateDog.vue
+++ oly/src/components/CreateDog.vue
@@ -1,9 +1,14 @@
 <template>
-    <div class="form-container">
-        <h2>Create a Dog</h2>
-        <form @submit.prevent="handleSubmit">
-            <label for="name">Dog Name</label>
-            <input type="text" id="name" v-model="dogName" required />
-        </form>
-    </div>
+  <div class="form-container">
+    <h2>Create Your Own Dog</h2>
+    <form @submit.prevent="handleSubmit">
+      <label for="name">Dog Name</label>
+      <input type="text" id="name" v-model="name" required />
+
+      <label for="url">Image URL</label>
+      <input type="url" id="url" v-model="url" required />
+
+      <button type="submit">Add Dog</button>
+    </form>
+  </div>
 </template>
